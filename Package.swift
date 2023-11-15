abeaf67b82177557cee8c3960b0649f28fde8280
Muhammad Shayan Zahid <shahyan.zahid@systemsltd.com>
2023-11-15 12:05:48 +0500

    added SmilesUtilities and SmilesFontsManager dependencies, added assets

--- Package.swift
+++ Package.swift
@@ -5,17 +5,31 @@ import PackageDescription
 
 let package = Package(
     name: "SmilesOrderTracking",
+    platforms: [
+        .iOS(.v13)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SmilesOrderTracking",
             targets: ["SmilesOrderTracking"]),
     ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/smilesiosteam/SmilesUtilities.git", branch: "main"),
+        .package(url: "https://github.com/smilesiosteam/SmilesFontsManager.git", branch: "main")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SmilesOrderTracking"),
+            name: "SmilesOrderTracking",
+            dependencies: [
+                .product(name: "SmilesUtilities", package: "SmilesUtilities"),
+                .product(name: "SmilesFontsManager", package: "SmilesFontsManager")
+            ]
+        ),
         .testTarget(
             name: "SmilesOrderTrackingTests",
             dependencies: ["SmilesOrderTracking"]),
